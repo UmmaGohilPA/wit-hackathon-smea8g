99e77645014e2e3477a5a3b42356ea2473fa0ffc
ummagohilpa <umma.gohil@paconsulting.com>
2022-09-08 15:59:03 +0100

    page four

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -5,7 +5,7 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
+import PageFour from "../images/Frame1.png";
 import "./Tab4.css";
 
 const Tab4: React.FC = () => {
@@ -17,12 +17,7 @@ const Tab4: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 4</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 4 page" />
+        <img src={PageFour} />
       </IonContent>
     </IonPage>
   );
